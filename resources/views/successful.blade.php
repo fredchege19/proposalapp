4208372ee7daf8ff063417aadb2186bbc0a406fe
Fredrick Chege <42474001+fredchege19@users.noreply.github.com>
2018-09-06 15:24:39 +0300

    new
    new

---
+++ resources/views/successful.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    
+   
+        <h3 style="text-align:center; color:green;">Your data has been received</h3><br>
+        
+        <hr>
+        <h4 style="text-align:center; color:grey;"> While you wait for a response from us, have a look at what our company is all about</h4>
+        <div class="video" style="margin-left:20% ">
+        <iframe width="530"  height="330" src="https://www.youtube.com/embed/jOlwW8cZjWo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        </div>
+@endsection
