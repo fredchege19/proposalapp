f5d0702a50aa80a1fbefd200fede8dc06d9d10ce
Fredrick Chege <42474001+fredchege19@users.noreply.github.com>
2018-09-20 16:41:41 +0300

    new
    new

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,5 +60,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'prevent-back-history' => \App\Http\Middleware\PreventBackHistory::class,
+        'admin' => 'App\Http\Middleware\Admin',
     ];
 }
