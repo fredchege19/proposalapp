a21a801a2fab64493308841019e33fc23a8209ea
Fredrick Chege <42474001+fredchege19@users.noreply.github.com>
2018-09-21 18:18:56 +0300

    new
    new

---
+++ resources/views/emails/rejected.blade.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Proposal Rejected Email</title>
+</head>
+ 
+<body>
+<h2></h2>
+<br/>
+We regret to inform you that your proposal has been rejected. Thanks for applying
+<br/>
+
+</body>
+ 
+</html>
