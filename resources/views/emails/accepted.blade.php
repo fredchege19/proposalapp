9c72f687b5faeb7a2153fc3fcf96d9c06a488e2a
Fredrick Chege <42474001+fredchege19@users.noreply.github.com>
2018-09-21 18:18:33 +0300

    new
    new

---
+++ resources/views/emails/accepted.blade.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Proposal Rejected Email</title>
+</head>
+ 
+<body>
+<h2></h2>
+<br/>
+Congratulations, your proposal has been verified and accepted
+<br/>
+
+</body>
+ 
+</html>
