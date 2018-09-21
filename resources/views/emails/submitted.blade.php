54bed857cef89a57425b08d5e02614c39ef2d9bd
Fredrick Chege <42474001+fredchege19@users.noreply.github.com>
2018-09-21 18:19:22 +0300

    new
    new

---
+++ resources/views/emails/submitted.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Email</title>
+</head>
+ 
+<body>
+
+<br/>
+We have received your proposal, we will get back to you shortly
+<br/>
+
+</body>
+ 
+</html>
