9bdb566720d3f1223e7ddd1627cf8acbaf6b5123
fredchege19 <42474001+fredchege19@users.noreply.github.com>
2018-10-05 15:31:43 +0300

    new
    new

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,29 @@
 <template>
   <div id="app">
-   
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
     <router-view/>
   </div>
-
-
 </template>
 
-
-<script>
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import Router from 'vue-router';
-
-
- export default{
-    name: 'app',
-    data(){
-      return{
-          
- }
-  },
-  methods:{
- loginGit(){
-   this.$action.get('doLogin')
- }
-
- }
- }
-</script>
-
-
-<style>
-#app{
-
-align: center;
-margin-left: auto;
-margin-right: auto;
-top:5%;
-height: 200px;
-
-width: 1000px;
-
-
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
